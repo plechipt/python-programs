639a45884169dabaa4bb33d24b2e19e264637ee7
Jakub Plecháč <loxtox74@gmail.com>
2021-02-25 12:25:58 +0100

    changed move object

--- pygame/snake.py
+++ pygame/snake.py
@@ -5,22 +5,20 @@ import pygame
 pygame.init()
 
 # Basic vars
-size = width, height = (600, 800)
-white = (255, 255, 255)
+size = width, height = (1200, 1000)
+black_background = (0, 0, 0)
 
 # Rectangle
-speed = 0.5
+speed = 1 
 x_rect = 250
 y_rect = 250
 size_of_rect = (50, 50)
-color_of_rect = (0, 0, 0)
+color_of_rect = (255, 255, 255)
 
-pygame.display.set_caption('Prvni appka v PyGame')
+pygame.display.set_caption('Snake Game')
 screen = pygame.display.set_mode(size)
 
-def move(x_rect, y_rect):
-    pressed = pygame.key.get_pressed()
-
+def move(pressed, x_rect, y_rect):
     if pressed[pygame.K_RIGHT]:
         x_rect += speed
     elif pressed[pygame.K_LEFT]:
@@ -32,18 +30,18 @@ def move(x_rect, y_rect):
 
     return x_rect, y_rect
 
+
 while True :
     events = pygame.event.get()
-
+    pressed = pygame.key.get_pressed()
+    screen.fill(black_background)
+    
     for event in events:
         action = event.type
-        
         # Exit
         if action == pygame.QUIT:
             sys.exit()
-    
-    screen.fill(white)
 
-    x_rect, y_rect = move(x_rect, y_rect)
+    x_rect, y_rect = move(pressed, x_rect, y_rect)
     rectangle = pygame.draw.rect(screen, color_of_rect, ((x_rect, y_rect), size_of_rect))
     pygame.display.update()
