03249a0609da9f6a6601f5120cc8e6fb22d3866d
Jakub Plecháč <plechac.k@gmail.com>
2023-10-17 21:53:06 +0200

    added stopwatch

--- programs/notes_generator.py
+++ programs/notes_generator.py
@@ -3,22 +3,22 @@ import time
 import random
 import keyboard
 
-natural_notes = ['A','B/Cb','C','D','E','F','G']
-sharp_notes = ['A#','C#','D#','F#','G#']
-flat_notes = ['Ab','Bb','Db','Eb', 'Gb']
+natural_notes = ['A','B','C','D','E','F','G']
+sharp_notes = ['A#','B#','C#','D#','E#','F#','G#']
+flat_notes = ['Ab','Bb','Cb','Eb','Db','Gb']
 
 sharp_mode = True
 timeout = 5
 first_round = True
 
 while True:
-    os.system('cls') # clear display
+    start_stopwatch = time.time()
 
     if first_round:
         user_input = int(input('\nEnter timeout in seconds for each note (you can also skip each note by pressing space or enter): ')) 
         timeout = user_input
 
-    input("\n Are you ready: ")
+    start_question = input("\n Are you ready: ")
     
     if sharp_mode:
         final_list = natural_notes + sharp_notes
@@ -41,6 +41,15 @@ while True:
                 break
         
         time.sleep(0.1)
+        
+    end_stopwatch = time.time()
+    final_time = round(end_stopwatch - start_stopwatch, 2)
+
+    os.system('cls') # clear display
+
+    print(f'It took you {final_time} seconds')
+
+    time.sleep(3)
 
     first_round = False
     sharp_mode = not sharp_mode
